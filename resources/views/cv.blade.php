ceef9929d65b634a6b561b29cdf5034ab9308827
Julian Vieser <mail@jvieser.de>
2021-01-19 19:41:34 +0100

    added whitespace

--- resources/views/cv.blade.php
+++ resources/views/cv.blade.php
@@ -9,7 +9,7 @@
         <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
             <div class="col p-4 d-flex flex-column position-static">
                 <strong class="d-inline-block mb-2 text-primary">Angestellt, seit Jan. 2016</strong>
-                <h3 class="mb-0">emarsys interactive services GmbH| Berlin</h3>
+                <h3 class="mb-0">emarsys interactive services GmbH | Berlin</h3>
                 <p class="card-text mb-auto">Webentwickler.</p>
             </div>
         </div>
