42e2621f294e36a8b0cf8a5aea5346e41861397f
Julian Vieser <mail@jvieser.de>
2021-01-19 19:48:11 +0100

    added / fixed noindex for imprint

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
         <meta name="description" content="{{ $pageMeta->description }}">
         <meta name="keywords" content="{{ $pageMeta->keywords }}">
         @endif
-        <meta name="robots" content="{{ Request::is('impressum') ? 'noindex' : 'index' }}">
+        <meta name="robots" content="{{ request()->routeIs('site.imprint') ? 'noindex' : 'index' }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
         <link rel="stylesheet" href="">
